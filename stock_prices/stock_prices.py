24418df7bcd6274b38c2a50e424acca66af9893c
Carlos Santana <carlossantana987@outlook.com>
2019-10-09 19:11:57 -0400

    stock price finished

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,44 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
 
+    buy_in_price = prices[0]
+
+
+    max_profit = -prices[0]
+
+    for i in prices:
+      # print(i)
+      if i - buy_in_price > max_profit and prices.index(i) != 0:
+        max_profit = i - buy_in_price
+
+      if i < buy_in_price:
+        buy_in_price = i
+
+    return max_profit
+
+
+
+
+
+
+  # bip = []
+  # mpsf = []
+
+  # for i in range(0,len(prices)-1):
+  #   if prices[i+1] > prices[i]:
+  #     bip.append(prices[i])
+  #   else:
+  #     mpsf.append(prices[i])
+  #
+  # max_profit =  mpsf[-1] - bip[0]
+  # return max_profit
+
+
+
+  # create foloop to run through each index in the array
+  # save the lowest number in the array to a variable
+  # find the largest number to sell at afterwards
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
