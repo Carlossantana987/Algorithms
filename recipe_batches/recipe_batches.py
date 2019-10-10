4783d53fd304761d4320cf119a1565c2ea6cf861
Carlos Santana <carlossantana987@outlook.com>
2019-10-10 10:58:23 -0400

    recipe batches finally done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,43 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+
+  if recipe.keys() > ingredients.keys():
+    return 0
+
+  batch = 0
+
+  while True:
+
+    for k,v in recipe.items():
+      print(f"ingred key: {ingredients[k]}")
+      ingredients[k] -= v
+      print(ingredients[k])
+
+    for k,v in ingredients.items():
+      if v < 0:
+        return batch
+      else:
+        pass
+
+    batch += 1
+
+
+
+
+
+
+
+
+  print(f"need: {ingredients_needed}")
+  print(f"have: {ingredients_have}")
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
